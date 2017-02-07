57f795240af162009aa0f54a68f65db1cadbe708
Isaac <isaacxpreston@gmail.com>
2017-02-06 22:30:10 -0800

    landing page done

--- searchRender.js
+++ searchRender.js
@@ -1,10 +1,11 @@
 document.getElementById('myForm').addEventListener( 'submit', prevent, false );
+document.getElementById('myForm2').addEventListener( 'submit', prevent, false );
 
 function prevent(e) {
   e.preventDefault();
 }
 
-const reRender = (e) => {
+const reRender = () => {
   let personality = document.getElementsByClassName("inputPersonality")[0].value.toUpperCase()
   if(types.indexOf(personality) !== -1) {
     history.pushState(null, null, personality);
@@ -13,10 +14,22 @@ const reRender = (e) => {
   document.getElementsByClassName("inputPersonality")[0].value = ""
 }
 
+const initRender = () => {
+  let personality = document.getElementsByClassName("inputPersonality2")[0].value.toUpperCase()
+  if(types.indexOf(personality) !== -1) {
+    history.pushState(null, null, personality);
+    renderFromUrl()
+  }
+  document.getElementsByClassName("inputPersonality2")[0].value = ""
+}
+
 const renderFromUrl = () => {
   let url = window.location.href.slice(22).toUpperCase()
   if(types.indexOf(url) !== -1) {
     init(url)
+    document.getElementById("landingBg").className = "display-hidden-none"
+    document.getElementById("landingContent").className = "display-hidden-none"
+    document.getElementById("searchBarLanding").className = "display-hidden-none"
   }
 }
 
