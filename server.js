291827e1b05d707fe1a33a223052b4d32e3549fa
Isaac <isaacxpreston@gmail.com>
2017-02-06 19:26:46 -0800

    notification popups

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const path = require('path')
-// const bodyParser = require('body-parser');
-// const morgan = require('morgan')
 const webpack = require('webpack');
 
 //const config = require('./webpack.config.js');
@@ -10,6 +8,7 @@ const webpack = require('webpack');
 // APP SETUP & MIDDLEWARE
 const app = express();
 app.use(express.static(__dirname));
+
 // const compiler = webpack(config);
 // app.use(require('webpack-dev-middleware')(compiler, {
 //   noInfo: true,
@@ -17,20 +16,8 @@ app.use(express.static(__dirname));
 // }));
 // app.use(require('webpack-hot-middleware')(compiler));
 
-app.get('/cubes', (req, res) => (
-  res.sendFile(path.resolve(__dirname, 'webgl_interactive_cubes.html'))
-));
-
-app.get('/youtube', (req, res) => (
-  res.sendFile(path.resolve(__dirname, 'css3d_youtube.html'))
-));
-
-app.get('/zoom', (req, res) => (
-  res.sendFile(path.resolve(__dirname, '03.02-zoom-camera-to-object.html'))
-));
-
 app.get('/', (req, res) => (
-  res.sendFile(path.resolve(__dirname, /*'../client/app',*/ 'index.html'))
+  res.sendFile(path.resolve(__dirname, 'index.html'))
 ));
 
 const PORT = process.env.PORT || 4000
