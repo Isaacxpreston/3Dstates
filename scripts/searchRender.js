8b13ad46deea869a8867defd9f6290c1223dc018
Isaac <isaacxpreston@gmail.com>
2017-02-12 00:09:56 -0800

    integrating new stylesheet

--- scripts/searchRender.js
+++ scripts/searchRender.js
@@ -21,9 +21,10 @@ const initRender = () => {
     resetStates(personality)
   }
   document.getElementsByClassName("inputPersonality2")[0].value = ""
-  document.getElementById("landingBg").className = "display-hidden-none"
+
   document.getElementById("landingContent").className = "display-hidden-none"
   document.getElementById("searchBarLanding").className = "display-hidden-none"
+
   disabled = false
   landingTween1.stop()
   landingTween2.stop()
@@ -32,15 +33,14 @@ const initRender = () => {
 //runs init if personality is in url
 const renderFromUrl = () => {
   // //for development
-  // let url = window.location.href.slice(22).toUpperCase()
+  let url = window.location.href.slice(22).toUpperCase()
 
   //for production
   // https://personality-data.herokuapp.com/
-  let url = window.location.href.slice(39).toUpperCase()
+  // let url = window.location.href.slice(39).toUpperCase()
   
   if(types.indexOf(url) !== -1) {
     init(url)
-    document.getElementById("landingBg").className = "display-hidden-none"
     document.getElementById("landingContent").className = "display-hidden-none"
     document.getElementById("searchBarLanding").className = "display-hidden-none"
   }
